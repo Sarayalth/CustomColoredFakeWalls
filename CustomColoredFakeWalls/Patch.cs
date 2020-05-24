22d996f95a4d67ef5b4a5571cae0137d9564edad
Saray <sarayalth@gmail.com>
2020-05-24 02:34:16 +0200

    Add project files.

---
+++ CustomColoredFakeWalls/Patch.cs
@@ -0,0 +1,30 @@
+﻿using BS_Utils.Utilities;
+using CustomColoredFakeWalls.UI;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CustomColoredFakeWalls
+{
+	[HarmonyPatch(typeof(ObstacleController))]
+	[HarmonyPatch("Init", MethodType.Normal)]
+	class TextSegmentedControlCellNew_Patch
+	{
+		static void Postfix(ObstacleController __instance, ref ObstacleData ____obstacleData, ref StretchableObstacle ____stretchableObstacle)
+		{
+			if (____obstacleData.duration < 0 && Settings.ApplyFastWallsColor)
+			{
+				var fastWallColor = ScriptableObject.CreateInstance<SimpleColorSO>();
+				fastWallColor.SetColor(Settings.FastWallsColor);
+				var _obstacleFrame = __instance.GetComponent<StretchableObstacle>().GetPrivateField<ParametricBoxFrameController>("_obstacleFrame");
+				__instance.GetComponent<StretchableObstacle>().SetSizeAndColor(_obstacleFrame.width, _obstacleFrame.height, _obstacleFrame.length, fastWallColor);
+			}
+			if (____obstacleData.width < 0 && Settings.ApplyFakeWallsColor)
+			{
+				var fakeWallColor = ScriptableObject.CreateInstance<SimpleColorSO>();
+				fakeWallColor.SetColor(Settings.FakeWallsColor);
+				var _obstacleFrame = __instance.GetComponent<StretchableObstacle>().GetPrivateField<ParametricBoxFrameController>("_obstacleFrame");
+				__instance.GetComponent<StretchableObstacle>().SetSizeAndColor(_obstacleFrame.width, _obstacleFrame.height, _obstacleFrame.length, fakeWallColor);
+			}
+		}
+	}
+}
---
+++ CustomColoredFakeWalls/UI/Settings.cs
@@ -0,0 +1,50 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using BS_Utils.Utilities;
+using UnityEngine;
+namespace CustomColoredFakeWalls.UI
+{
+	class Settings : PersistentSingleton<Settings>
+	{
+		public static Config config;
+		public void Awake()
+		{
+			config = new Config("CustomColoredFakeWalls");
+		}
+
+		[UIValue("fast-walls-color")]
+		public static Color FastWallsColor {
+			get {
+				var color = config.GetString("CustomColoredFakeWalls", "FastWallsColor", "#FFEB04FF");
+				Color parsedColor;
+				ColorUtility.TryParseHtmlString(color, out parsedColor);
+				return parsedColor;
+			}
+			set {
+				config.SetString("CustomColoredFakeWalls", "FastWallsColor", "#" + ColorUtility.ToHtmlStringRGBA(value));
+			}
+		}
+		[UIValue("fast-walls-color-bool")]
+		public static bool ApplyFastWallsColor {
+			get => config.GetBool("CustomColoredFakeWalls", "ApplyFastWallsColor", false);
+			set => config.SetBool("CustomColoredFakeWalls", "ApplyFastWallsColor", value);
+		}
+
+		[UIValue("fake-walls-color")]
+		public static Color FakeWallsColor {
+			get {
+				var color = config.GetString("CustomColoredFakeWalls", "FakeWallsColor", "#00FF00FF");
+				Color parsedColor;
+				ColorUtility.TryParseHtmlString(color, out parsedColor);
+				return parsedColor;
+			}
+			set {
+				config.SetString("CustomColoredFakeWalls", "FakeWallsColor", "#" + ColorUtility.ToHtmlStringRGBA(value));
+			}
+		}
+		[UIValue("fake-walls-color-bool")]
+		public static bool ApplyFakeWallsColor {
+			get => config.GetBool("CustomColoredFakeWalls", "ApplyFakeWallsColor", true);
+			set => config.SetBool("CustomColoredFakeWalls", "ApplyFakeWallsColor", value);
+		}
+	}
+}
